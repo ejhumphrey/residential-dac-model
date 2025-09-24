90aecf5f4d75eca3f3c8dbd85eb2d1d2363b16be
Eric J. Humphrey <ejhumphrey@Mac.lan>
2025-09-23 22:13:57 -0400

    Updated for pwd and gh deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import react from '@astrojs/react';
-// https://astro.build/config
+
+// The output needs to be 'static' for GitHub Pages
 export default defineConfig({
+  output: 'static', 
+  // IMPORTANT: Replace with your details
+  site: 'https://ejhumphrey.github.io',
+  base: '/residential-dac-model/', // Must start and end with a slash
   integrations: [react()]
 });
